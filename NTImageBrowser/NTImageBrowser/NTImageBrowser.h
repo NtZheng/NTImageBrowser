c175375353f266e9c00209b5c0b2d4e525b6cf89
NtZheng <2745674085@qq.com>
2016-10-05 14:51:52 +0800

    搭建好了基本框架..

---
+++ NTImageBrowser/NTImageBrowser/NTImageBrowser.h
@@ -0,0 +1,13 @@
+//
+//  NTImageBrowser.h
+//  NTImageBrowser
+//
+//  Created by Nineteen on 10/5/16.
+//  Copyright © 2016 Nineteen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NTImageBrowser : NSObject
+
+@end
---
+++ NTImageBrowser/NTImageBrowser/NTImageBrowser.m
@@ -0,0 +1,13 @@
+//
+//  NTImageBrowser.m
+//  NTImageBrowser
+//
+//  Created by Nineteen on 10/5/16.
+//  Copyright © 2016 Nineteen. All rights reserved.
+//
+
+#import "NTImageBrowser.h"
+
+@implementation NTImageBrowser
+
+@end
